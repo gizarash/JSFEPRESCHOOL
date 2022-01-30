d1ac5cb3b573040f2710299b4af9e0336ea36b90
Rashid Gizatullin <gizarash@gmail.com>
2022-01-30 21:00:48 +0600

    feat: add season switch functionality to portfolio section

--- portfolio/assets/js/index.js
+++ portfolio/assets/js/index.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
 const mobMenu = document.querySelector('.mob-menu');
 const mobMenuList = document.querySelector('.mob-menu-list');
+const portfolioBtns = document.querySelector('.season-switch');
+const portfolioImages = document.querySelectorAll('.portfolio-image');
 
 function toggleMenu() {
   hamburger.classList.toggle('is-active');
@@ -14,7 +16,26 @@ function closeMenu(event) {
   }
 }
 
+function changeImage(event) {
+  if(event.target.classList.contains('btn')) {
+    const season = event.target.dataset.season;
+    preloadSeasonImages(season);
+    portfolioImages.forEach((img, index) => img.src = `./assets/img/${season}/${index + 1}.jpg`);
+    document.querySelectorAll('.portfolio-btn').forEach((btn) => btn.classList.remove('active'));
+    event.target.classList.add('active');
+  }
+}
+
+function preloadSeasonImages(season) {
+  for(let i = 1; i <= 6; i++) {
+    const img = new Image();
+    img.src = `./assets/img/${season}/${i}.jpg`;
+  }
+}
+
 hamburger.addEventListener('click', toggleMenu);
 mobMenuList.addEventListener('click', closeMenu);
+portfolioBtns.addEventListener('click', changeImage);
+
 
 console.log('Все пункты выполнены полностью!')
