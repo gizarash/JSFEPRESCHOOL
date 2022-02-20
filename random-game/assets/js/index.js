750f6522feae4674df7c43bb88d563f23c69a611
Rashid Gizatullin <gizarash@gmail.com>
2022-02-20 17:06:44 +0600

    feat: add bird's falling and taking off ability

--- random-game/assets/js/index.js
+++ random-game/assets/js/index.js
@@ -6,6 +6,8 @@ const bird = new Image();
 const topPipe = new Image();
 const bottomPipe = new Image();
 const pipesGap = 100;
+const gravity = 1.5;
+const takeOffHeight = 25;
 let posX = 10;
 let posY = 250;
 
@@ -15,6 +17,11 @@ bird.src = "./assets/img/bird.png";
 topPipe.src = "./assets/img/topPipe.png";
 bottomPipe.src = "./assets/img/bottomPipe.png";
 
+document.addEventListener('keydown', (e) => {
+  if(e.code == 'Space') {
+    posY -= takeOffHeight;
+  }
+});
 
 function draw() {
   ctx.drawImage(bg, 0, 0);
@@ -22,6 +29,13 @@ function draw() {
   ctx.drawImage(bottomPipe, 100, topPipe.height + pipesGap);
   ctx.drawImage(fg, 0, cvs.height - fg.height);
   ctx.drawImage(bird, posX, posY);
+
+  posY += gravity;
+  requestAnimationFrame(draw);
+}
+
+function takeOff() {
+  posY -= takeOffHeight;
 }
 
 bottomPipe.onload = draw;
